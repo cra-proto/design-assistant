9289aee86d347d1e2663320cdc5617fdcf93c5b3
Amber <amber.leblanc@cra-arc.gc.ca>
2025-11-18 21:34:54 -0500

    Fix payload

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -125,13 +125,14 @@ resource "aws_apigatewayv2_integration" "callback" {
   api_id           = aws_apigatewayv2_api.api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.github_callback.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "callback" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "POST /auth/github/callback"
   target    = "integrations/${aws_apigatewayv2_integration.callback.id}"
-  payload_format_version = "2.0"
 }
 
 # Lambda permissions for API Gateway
