cc27596fe54b4b185301a0227b18e79d076f0c68
Amber <amber.leblanc@cra-arc.gc.ca>
2025-12-05 10:09:43 -0500

    Rotate between 404 messages

--- src/app/views/standalone/not-found.component.ts
+++ src/app/views/standalone/not-found.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
   selector: 'aida-not-found',
   imports: [TranslateModule],
   template: `
-    <span [innerHTML]="'404.message' | translate"></span>
+  
+    <span [innerHTML]="random404Key | translate"></span>
   `,
   styles: ``
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
 
+  random404Key: string = '404.message.0';
+
+  ngOnInit() {
+    const randomIndex = Math.floor(Math.random() * 6);
+    this.random404Key = `404.message.${randomIndex}`;
+    console.log('Selected 404 message key:', this.random404Key);
+  }
 }
