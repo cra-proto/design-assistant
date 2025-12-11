9ea1be961d685267920e6d449010c85e03c0526c
Amber <amber.leblanc@cra-arc.gc.ca>
2025-12-11 15:39:13 -0500

    Update api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://4k8slfnd5m.execute-api.ca-central-1.amazonaws.com/dev'
+    apiUrl: 'https://6d39x0uozc.execute-api.ca-central-1.amazonaws.com/dev'
 };
