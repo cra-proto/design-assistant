51f4e6f6d5a21a76f143cffd27bfa0342c901daf
Amber <amber.leblanc@cra-arc.gc.ca>
2025-12-11 15:30:08 -0500

    Update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://b3hiyzd4b0.execute-api.ca-central-1.amazonaws.com/production'
+    apiUrl: 'https://4k8slfnd5m.execute-api.ca-central-1.amazonaws.com/production'
 };
