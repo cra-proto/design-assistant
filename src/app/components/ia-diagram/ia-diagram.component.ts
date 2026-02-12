4a5038d63d95713ba1284ec8f98d23eed737ae08
Amber <amber.leblanc@cra-arc.gc.ca>
2026-02-12 15:37:04 -0500

    Add tooltips for orphans

--- src/app/components/ia-diagram/ia-diagram.component.ts
+++ src/app/components/ia-diagram/ia-diagram.component.ts
@@ -1,12 +1,10 @@
 import { Component, inject, computed } from '@angular/core';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 // PrimeNG modules
 import { OrganizationChartModule } from 'primeng/organizationchart';
 import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 // Services
 import { ProjectStateService } from '../../services/project-state.service';
@@ -14,15 +12,13 @@ import { IaDiagramService } from './ia-diagram.service';
 
 @Component({
   selector: 'aida-ia-diagram',
-  imports: [
-    CommonModule, FormsModule, TranslateModule,
-    OrganizationChartModule, ButtonModule
+  imports: [TranslateModule,
+    OrganizationChartModule, ButtonModule, TooltipModule
   ],
   templateUrl: './ia-diagram.component.html',
   styleUrl: './ia-diagram.component.css'
 })
 export class IaDiagramComponent {
-  router = inject(Router)
   projectState = inject(ProjectStateService)
   iaDiagram = inject(IaDiagramService);
   projectTree = computed(() => this.projectState.getProject().projectData);
