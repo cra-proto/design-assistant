2ff87949361193107bd28873e799cae1b491d7ab
Amber <amber.leblanc@cra-arc.gc.ca>
2026-03-06 12:21:24 -0500

    Add openrouter function url

--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -5,7 +5,7 @@ export const environment = { // Same infrastructure as development, only differe
     apiGateway: 'https://nappswkoie.execute-api.ca-central-1.amazonaws.com/dev',
     dynamodbFunctionUrl: 'https://chi2rsccsm5tsbq3dzqez735gu0pxnlj.lambda-url.ca-central-1.on.aws/',
     airtableFunctionUrl: 'https://i7vbe2ntgsh26rvibknsi4zuia0rmpyn.lambda-url.ca-central-1.on.aws/',
-    openrouterFunctionUrl: 'https://your-function-url.lambda-url.ca-central-1.on.aws/',
+    openrouterFunctionUrl: 'https://lh3cjcmhvvekco7nkioztgxnxq0pcisv.lambda-url.ca-central-1.on.aws/',
     defaultOrg: 'cra-proto',
     templateOrg: 'proto-cra', //for accessing core-prototype
 };
