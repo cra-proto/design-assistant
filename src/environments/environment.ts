810e00a60ad02d8ae17a5e00542642b4659e1e94
Amber <amber.leblanc@cra-arc.gc.ca>
2026-06-12 20:49:23 -0400

    Add prod url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     dynamodbFunctionUrl: 'https://jorsotuenahud6d5dlloupex440spkyh.lambda-url.ca-central-1.on.aws/',
     airtableFunctionUrl: 'https://xohirz6dqj47ub3jrayhjv52ja0qmyfu.lambda-url.ca-central-1.on.aws/',
     openrouterFunctionUrl: 'https://bdib56cgfipvlfjnry66clvzdy0ycvpa.lambda-url.ca-central-1.on.aws/',
-    usageFunctionUrl: '',
+    usageFunctionUrl: 'https://pkme7nxqensyvtmag3ajyhi4a40eemsv.lambda-url.ca-central-1.on.aws/',
     defaultOrg: 'cra-proto',
     templateOrg: 'cra-proto', //for accessing core-prototype
 };
